6723d25e8d2b82d682feee5f8909993c3b8b4227
Sherman Mui <sherman@meatchunks.com>
2019-05-04 17:39:34 -0700

    Add awesome client layer

--- keyboards/bpiphany/sixshooter/keymaps/lavastone/keymap.c
+++ keyboards/bpiphany/sixshooter/keymaps/lavastone/keymap.c
@@ -16,42 +16,37 @@
 #include QMK_KEYBOARD_H
 #include "timer.h"
 
-#define _BL 0
-#define _FN 1
+#define _BL         0
+#define _AWM_CLIENT 1
+#define _TM         2
 #define MOD4 KC_RGUI
 
 #define MAX_KEYPRESS_DIFFERENCE 200 /* In milliseconds */
+
 // Define keyboard specific keycodes for controlling on/off for all LEDs as they
 // are all on different pins with this PCB, rather than a single backlight pin
 enum custom_keycodes {
   SS_100 = SAFE_RANGE,
   SS_010,
   SS_001,
-  SS_LON,
-  SS_LOFF
+  SS_TMUX_PASTE
 };
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /* Base Layer: Media Keys
-     * ,-----------.
-     * |FN | V-| V+|
-     * |---+---+---|
-     * |Prv|Ply|Nxt|
-     * `-----------'
-     */
-  [_BL] = LAYOUT( /* Base */
-    RGUI(KC_H), RGUI(KC_L), MO(_FN), \
-    SS_100, SS_010, SS_001
+  /* Base Layer: AwesomeWM screen controls
+   */
+  [_BL] = LAYOUT
+  ( /* Base */
+   RGUI(KC_H), RGUI(KC_L), TO(_AWM_CLIENT),     \
+   SS_100,     SS_010,     SS_001
   ),
-    /* FN Layer: LED control
-     * ,-----------.
-     * |FN | V-| V+|
-     * |---+---+---|
-     * |Prv|Ply|Nxt|
-     * `-----------'
-     */
-  [_FN] = LAYOUT(
-    KC_TRNS, SS_LON, SS_LOFF, \
-    KC_NO,   KC_NO,  KC_NO
+
+  /* AwesomeWM Client Layer: Client controls
+   */
+  [_AWM_CLIENT] = LAYOUT
+  (
+   SS_TMUX_PASTE, RGUI(KC_ENTER), TO(_BL),      \
+   RGUI(KC_K),    RGUI(KC_J),     RGUI(KC_N)
   ),
 };
 
@@ -84,35 +79,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // Check for custom keycodes for turning on and off LEDs
     switch(keycode) {
     case SS_100:
-      binary_number_timers[2] = timer_read();
-      break;
+      binary_number_timers[2] = record->event.time;
+      return false;
     case SS_010:
-      binary_number_timers[1] = timer_read();
-      break;
+      binary_number_timers[1] = record->event.time;
+      return false;
     case SS_001:
-      binary_number_timers[0] = timer_read();
-      break;
-    case SS_LON:
-      sixshooter_led_all_on();
-      break;
-    case SS_LOFF:
-      sixshooter_led_all_off();
-      break;
+      binary_number_timers[0] = record->event.time;
+      return false;
+
+    case SS_TMUX_PASTE:
+      SEND_STRING(SS_LCTRL("]") "P");
+      return false;
     }
   }
-  else {
+  else { // Released
     uint16_t liftoff_time = 0;
     switch(keycode) {
     case SS_100:
     case SS_010:
     case SS_001:
-      liftoff_time = timer_read();
+      liftoff_time = record->event.time;
       uint8_t number = number_from_timers(liftoff_time);
-      uint32_t kc = KC_1;
+      uint16_t kc = KC_1;
       if (number < 1 || number > 7) {
         break;
       }
-      kc += number -1;
+      kc += number - 1;
       register_code(MOD4);
       register_code(kc);
       unregister_code(kc);
@@ -136,9 +129,14 @@ uint32_t layer_state_set_user(uint32_t state) {
   case _BL:
     sixshooter_led_all_on();
     break;
-    /* case _AWM_SCREENS: */
-    /*   sixshooter_led_0_off(); */
-    /*   break; */
+  case _AWM_CLIENT:
+    sixshooter_led_0_on();
+    sixshooter_led_1_off();
+    sixshooter_led_2_on();
+    sixshooter_led_3_on();
+    sixshooter_led_4_off();
+    sixshooter_led_5_on();
+    break;
   }
   return state;
 }
