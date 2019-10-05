6917f838cf73a5e459adf60f72cc97bd6a0d2c74
Sherman Mui <sherman@meatchunks.com>
2019-10-04 17:52:28 -0700

    lots of usability fixes. it's pretty good now

--- keyboards/keebio/iris/keymaps/sherrman/config.h
+++ keyboards/keebio/iris/keymaps/sherrman/config.h
@@ -29,6 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define IGNORE_MOD_TAP_INTERRUPT
 
-#define AUTO_SHIFT_TIMEOUT 95
-#define ONESHOT_TAP_TOGGLE 5  /* Tapping this number of times holds the key until tapped this number of times again. */
-#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
+#define ONESHOT_TAP_TOGGLE 5 /* Tapping this number of times holds the key until tapped this number of times again. */
+#define ONESHOT_TIMEOUT 5000 /* Time (in ms) before the one shot key is released */
+
+#define TAPPING_TERM 200
--- keyboards/keebio/iris/keymaps/sherrman/keymap.c
+++ keyboards/keebio/iris/keymaps/sherrman/keymap.c
@@ -7,6 +7,7 @@ enum iris_layers { _QWERTY, _LOWER, _RAISE, _ADJUST };
 #define LOWER TG(_LOWER)
 #define RAISE MO(_RAISE)
 #define RSFT_BSLS RSFT_T(KC_BSLS)
+#define LCTL_BSLS LCTL_T(KC_BSLS)
 #define LSFT_ESC LSFT_T(KC_ESC)
 
 #define SFM_BASE_BACKLIGLHT_LEVEL 2
@@ -43,36 +44,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //     `/~ |   1    |   2    |   3    |    4   |    5   |                          |    6   |   7    |   8    |   9    |   0    |   _/-  |
     KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-  //|  TAB   |   Q    |   W    |   E    |    R   |    T   |                          |    Y   |   U    |   I    |   O    |   P    |   +/=  |
-     KC_TAB,   KC_Q,    KC_W,     KC_E,   KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
+  //|  TAB   |   Q    |   W    |   E    |    R   |    T   |                          |    Y   |   U    |   I    |   O    |   P    |  BKSPC |
+     KC_TAB,   KC_Q,    KC_W,     KC_E,   KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
   //| RAISE  |   A    |   S    |   D    |    F   |    G   |                          |    H   |   J    |   K    |   L    |   :/;  |   "/'  |
-      RAISE,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    LSFT_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-  //| LOWER  |   Z    |   X    |   C    |    V   |    B   | COMMAND         | ALT    |    N   |   M    |   </,  |  >/.   |  ?-/   | ↑ |-\  |
-      LOWER,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,         KC_LALT,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH, RSFT_BSLS,
+  //| LOWER  |   Z    |   X    |   C    |    V   |    B   | COMMAND         | ALT    |    N   |   M    |   </,  |  >/.   |  ?-/   | SHIFT  |
+      LOWER,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,         KC_LALT,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH, TD(X_SHFT_CAPS),
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-  //                              SFT ESC   |  CTRL  | BKSPACE|                 | SPACE  |  ENTER | SHIFT  |
-                                   LSFT_ESC, KC_LCTL, KC_BSPC,                   KC_SPC,  KC_ENT,  TD(X_SHFT_CAPS)
+  //                              SFT ESC   |  CTRL  | BKSPACE|                 | SPACE  |  ENTER |   -\   |
+                                     RAISE,  KC_LCTL, KC_BSPC,                   KC_SPC,  KC_ENT,  LCTL_BSLS
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 //KC_LALT  ALT
   [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-  //|  `/~   |   F1   |   F2   |   F3   |    F4  |   F5   |                          |   F6   |   F7   |   F8   |   F9   |   F10  |   F11  |
-     KC_TRNS,  KC_F1,    KC_F2,  KC_F3,   KC_F4,   KC_F5,                              KC_F6,    KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,
+  //|  F12   |   F1   |   F2   |   F3   |    F4  |   F5   |                          |   F6   |   F7   |   F8   |   F9   |   F10  |   F11  |
+      KC_F12,  KC_F1,    KC_F2,  KC_F3,   KC_F4,   KC_F5,                              KC_F6,    KC_F7,   KC_F8,  KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-  //|  TAB   |    !   |    @   |   {    |   }    |    |   |                          |        |  PGUP  |   ↑    |  PGDN  |        |   F12  |
-     KC_TRNS, KC_EXLM,   KC_AT,  KC_LCBR, KC_RCBR, KC_PIPE,                           XXXXXXX, KC_PGUP,  KC_UP,  KC_PGDN, XXXXXXX, KC_F12,
+  //|  TAB   |    !   |    @   |   {    |   }    |    |   |                          |   <|   |  PGUP  |   ↑    |  PGDN  |   |>   |   +/=  |
+     KC_TRNS, KC_EXLM,   KC_AT,  KC_LCBR, KC_RCBR, KC_PIPE,                           KC_MPRV, KC_PGUP,  KC_UP,  KC_PGDN, KC_MNXT,  KC_EQL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-  //|  RAISE |    #   |    $   |   (    |   )    |    `   |                          |   ?    |    ←   |   ↓    |    →   |        |  VOLUP |
-      KC_TRNS,  KC_HASH, KC_DLR, KC_LPRN, KC_RPRN,  KC_GRV,                            KC_QUES, KC_LEFT,KC_DOWN, KC_RGHT, XXXXXXX, KC__VOLUP,
+  //|  RAISE |    #   |    $   |   (    |   )    |    `   |                          |   ?    |    ←   |   ↓    |    →   |  || >  |  VOLUP |
+      KC_TRNS,  KC_HASH, KC_DLR, KC_LPRN, KC_RPRN,  KC_GRV,                            KC_QUES, KC_LEFT,KC_DOWN, KC_RGHT, KC_MPLY, KC__VOLUP,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
   //|        |    %   |    ^   |   [    |   ]    |    ~   | COMMAND         |   ALT  |    &   |   INS  |  HOME  |  END   |  MUTE  |  VOLDN |
      XXXXXXX, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD,  KC_TRNS,          KC_TRNS, KC_AMPR, KC_INS, KC_HOME,  KC_END, KC__MUTE,KC__VOLDOWN,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-  //                               |  ESC   | RCTRL  | BKSPACE|                 | SPACE  |  RCTRL | SHIFT  |
-                                    KC_TRNS, KC_RCTL, KC_TRNS,                   KC_TRNS,  KC_RCTL, KC_TRNS
+  //                               |  ESC   |  CTRL  | BKSPACE|                 | SPACE  |  RCTRL | SHIFT  |
+                                    KC_TRNS, KC_LCTL, KC_TRNS,                   KC_TRNS, KC_RCTL, KC_RSFT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -91,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,         KC_TRNS,  XXXXXXX, KC_P0,   KC_PDOT, KC_PENT, KC_PEQL, XXXXXXX,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
   //                               |  ESC   | RCTRL  | BKSPACE|                 | SPACE  |  RCTRL | SHIFT  |
-                                    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
+                                    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_RCTL, KC_RSFT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -110,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,         KC_TRNS,  XXXXXXX, KC_P0,   KC_PDOT, KC_PENT, KC_PEQL, XXXXXXX,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
   //                               |  CTRL  | COMMAND| BKSPACE|                 | SPACE  | ENTER  | SHIFT  |
-                                    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS
+                                    KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_RSFT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
     // clang-format on
@@ -181,23 +182,24 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 /*  } */
 
 uint32_t layer_state_set_user(uint32_t state) {
-    switch (biton32(state)) {
+    switch (get_highest_layer(state)) {
         case _QWERTY:
             backlight_enable();
             backlight_level(SFM_BASE_BACKLIGLHT_LEVEL);
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT + 3);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_SWRIL + 4);
             break;
         case _LOWER:
-            rgblight_sethsv_noeeprom(HSV_GREEN);
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_SNAKE + 3);
             break;
         case _RAISE:
-            rgblight_sethsv_noeeprom(HSV_BLUE);
+            /* rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT); */
+            rgblight_mode_noeeprom(RGBLIGHT_MODE_RAINBOW_MOOD + 2);
             break;
         /* case _ADJUST: */
         /*   rgblight_sethsv_noeeprom(HSV_ORANGE); */
         /*   break; */
         default:
-            rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_GRADIENT + 3);
+            rgblight_sethsv_noeeprom(RGB_TURQUOISE);
             break;
     }
     return state;
